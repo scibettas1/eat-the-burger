bff3f305b42b66605eef9cb94c9a1abec92be876
Sam Scibetta <sammieeccentric@gmail.com>
2020-11-28 16:16:00 -0500

    updated and connected the css

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const path = require("path");
 const fs = require("fs")
 
 module.exports = function (app) {
+
+  app.use(express.static(__dirname + '/public'));
+
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
   });
