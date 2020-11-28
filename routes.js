c15ef82a5a8909d2fd1ceeed1b34e7f904dd7c0d
Sam Scibetta <sammieeccentric@gmail.com>
2020-11-28 15:50:18 -0500

    my first commit

---
+++ routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs")
+
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+// Dependencies
+const express = require("express");
+
+// Sets up the Express App
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Reads the routes file
+require('./routes.js')(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
