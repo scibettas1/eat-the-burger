dc236cabd149bdf87682a3faaf19d06bb891ebfe
Sam Scibetta <sammieeccentric@gmail.com>
2020-12-06 17:44:08 -0500

    connecting the jaws db

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
+    user: "w6mg3bsot4tr2s04",
+    password: "a3ww7q0uvlw4da4y",
+    database: "oowfizrcul73tcya"
   });
 }
 // Make connection.
